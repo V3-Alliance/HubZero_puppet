7aa0f12a8e48cb881f8c6fb069a8b805b94f5526
Martin Paulo <martin.paulo@gmail.com>
2014-12-17 15:12:34 +1100

    Added a todo about the extra root users: and possibly removing them.

--- puppet/modules/hubzero-database/manifests/init.pp
+++ puppet/modules/hubzero-database/manifests/init.pp
@@ -2,7 +2,18 @@ class hubzero-database (
   $mysql_password,
 ){
 
-  class { 'mysql::server':
+# TODO: This gives the following root users: we should possibly clear the two without passwords?
+# mysql> select Host,Password from user where User='root';
+# +------------+-------------------------------------------+
+# | Host       | Password                                  |
+# +------------+-------------------------------------------+
+# | localhost  | *935709B6DFFF2CEA9CDC9B3448E49178478D1774 |
+# | hubzero.tk |                                           |
+# | 127.0.0.1  |                                           |
+# +------------+-------------------------------------------+
+
+
+class { 'mysql::server':
     root_password => "$mysql_password",
   }
 
