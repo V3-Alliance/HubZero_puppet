4f3697a3c474d981787c4c97f2a9e150a93e94d4
Martin Paulo <martin.paulo@gmail.com>
2014-12-17 16:00:16 +1100

    Noted that mail seemed to be working... But that we still need to configure it the way in which hubzero wants it.

--- puppet/modules/exim4/manifests/init.pp
+++ puppet/modules/exim4/manifests/init.pp
@@ -1,5 +1,6 @@
 class exim4 {
   # based on http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/
+  # TODO: but now need to set up the correct response file.
 
   package { "exim4":
     require => File ["/var/cache/debconf/exim4.seeds"],
