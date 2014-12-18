11b6837ba369e2a740652405c754a823932653f1
Martin Paulo <martin.paulo@gmail.com>
2014-12-18 14:18:48 +1100

    First cut at adding the seeding of the slapd admin password.

--- puppet/modules/hubzero/manifests/init.pp
+++ puppet/modules/hubzero/manifests/init.pp
@@ -2,5 +2,5 @@ class hubzero {
   include hubzero-database
   # include exim4
   # include cms
-
+  include open-ldap
 }
--- puppet/modules/open-ldap/manifests/init.pp
+++ puppet/modules/open-ldap/manifests/init.pp
@@ -2,8 +2,17 @@ class open-ldap (
   $slapd_password,
 ){
 
+
+# Install the preseed packages
   package { "slapd":
     ensure => latest,
+    require => File ["/var/cache/debconf/slapd.seeds"],
+    responsefile => "/var/cache/debconf/slapd.seeds",
+  }
+
+  file {"/var/cache/debconf/slapd.seeds":
+    content => template("ldap/slapd.seeds"),
+    ensure => present,
   }
 
   package { "hubzero-openldap":
