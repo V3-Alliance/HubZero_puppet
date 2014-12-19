d51b81a0c185b0803ad66e65d857cf5b1583042b
Martin Paulo <martin.paulo@gmail.com>
2014-12-19 14:18:00 +1100

    Added configuration of ldap

--- puppet/modules/open-ldap/manifests/init.pp
+++ puppet/modules/open-ldap/manifests/init.pp
@@ -22,6 +22,21 @@ class open-ldap (
     require => [Package["slapd"]],
   }
 
-#/usr/bin/hzldap
+  exec { "initialize ldap":
+    command      => "/usr/bin/hzldap init 2>&1 | tee /tmp/ldap_details.txt",
+    require      => Package ["hubzero-openldap"],
+    creates      => "/tmp/ldap_details.txt"
+  }
 
+  exec { "enable ldap":
+    command      => "hzcms configure ldap --enable",
+    subscribe    => Exec["initialize ldap"]
+  }
+
+# somehow, need to:
+#  Go to administrator section of your site (/administrator),
+#  go to Site->Maintenance->LDAP and press the Export Users and Export Groups buttons
+#  in order to export all CMS users/groups
+
+# to test: # getent passwd
 }
