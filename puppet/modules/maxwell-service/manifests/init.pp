e71595f82a009b476d91dc74a14b93f40c913a01
Martin Paulo <martin.paulo@gmail.com>
2014-12-22 12:31:47 +1100

    Added in the Maxwell Service

---
+++ puppet/modules/maxwell-service/manifests/init.pp
@@ -0,0 +1,19 @@
+#<h2>Maxwell Service</h2>
+#<p><b>See:</b> <a href="https://hubzero.org/documentation/1.1.0/installation/Setup.maxwell_service">https://hubzero.org/documentation/1.1.0/installation/Setup.maxwell_service</a>
+class maxwell-service {
+
+  package { "hubzero-mw-service":
+    ensure => latest,
+  }
+  ->
+  exec { "create template":
+    command      => "/usr/bin/mkvztemplate amd64 squeeze manny",
+    require      => Package["hubzero-mw-service"],
+  }
+  ->
+  exec { "enable maxwell":
+    command      => "/usr/bin/hzcms configure mw-service --enable",
+    require      => [Package["hubzero-mw-service"], Package["hubzero-cms"]],
+  }
+
+}
