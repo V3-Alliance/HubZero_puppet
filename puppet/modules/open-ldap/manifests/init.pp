2e9972d1f4e2411480eef7c05b981c8ad7736ab8
Martin Paulo <martin.paulo@gmail.com>
2014-12-22 12:16:38 +1100

    Added some more comments

--- puppet/modules/open-ldap/manifests/init.pp
+++ puppet/modules/open-ldap/manifests/init.pp
@@ -1,14 +1,26 @@
 # <h2>OpenLDAP</h2>
 # <p>Install and configure LDAP.
 # <p><b>See:</b> <a href="https://hubzero.org/documentation/1.1.0/installation/Setup.openldap">https://hubzero.org/documentation/1.1.0/installation/Setup.openldap</a>
+# <p>Currently get the following message:
+# <pre>/Exec[enable ldap]/returns:
+#     syncing user 'admin' to ldap#033[0m
+#     pw = pwd.getpwnam('admin')#033[0m
+#     KeyError: 'getpwnam(): name not found: admin'#033[0m
+#</pre>
+# <p>I'm not sure if this is meant to happen or not?
+# <p>If this class is not good enough, try https://github.com/jfernandezr/puppet-ldap/blob/master/manifests/server.pp
+# <p>TODO: somehow, need to:
+#  <ul>
+#<li>Go to administrator section of your site (/administrator),</li>
+#<li>go to Site->Maintenance->LDAP and press the Export Users and Export Groups buttons
+#  in order to export all CMS users/groups</li>
+#</ul>
+
+# to test: # getent passwd
 class open-ldap (
   $slapd_password,
 ){
 
-# if not good enough, try https://github.com/jfernandezr/puppet-ldap/blob/master/manifests/server.pp
-
-# after install can test with:
-# sudo slapcat
   package { "slapd":
     ensure       => latest,
     require      => File ["/var/cache/debconf/slapd.seeds"],
@@ -37,10 +49,4 @@ class open-ldap (
     require      => [Package ["hubzero-cms"], Package["hubzero-openldap"]],
   }
 
-# somehow, need to:
-#  Go to administrator section of your site (/administrator),
-#  go to Site->Maintenance->LDAP and press the Export Users and Export Groups buttons
-#  in order to export all CMS users/groups
-
-# to test: # getent passwd
 }
