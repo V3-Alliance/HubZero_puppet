98a5f1fa995d7ee387ddd8a5c03191a704e04479
Martin Paulo <martin.paulo@gmail.com>
2014-12-22 15:14:44 +1100

    Added relevant puppet modules

---
+++ puppet/hubzero_1_2/manifests/init.pp
@@ -0,0 +1,22 @@
+class hubzero {
+#  include hubzero-database
+#  # include exim4
+#  include cms
+#  include open-ldap
+#  # skip ssh and sftp
+#  include webdav
+#  include subversion
+#  include trac
+#  include forge
+#  include openvz
+#  include firewall
+#  # include maxwell-service # Is slightly broken...
+#  # include maxwell-client
+#  include vncproxy
+#  # include telequotad
+#  include workspace
+#  include metrics
+#  # include rappture
+#  include filexfer
+#  # include submit
+}
