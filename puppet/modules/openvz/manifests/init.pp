d671931c3f9e94f1efbff08cbf7b0f17ed4643f0
Martin Paulo <martin.paulo@gmail.com>
2014-12-23 13:40:56 +1100

    Added OpenVZ installation to all versions of HubZero

--- puppet/modules/openvz/manifests/init.pp
+++ puppet/modules/openvz/manifests/init.pp
@@ -1,12 +1,31 @@
 # <h2>OpenVZ</h2>
 # <p>Installs the OpenVZ containers.
+# <p>See:
+# <ul>
+#   <li><a href="https://hubzero.org/documentation/1.1.0/installation/Setup.openvz">1.1 Install Instructions</a>
+#   <li><a href="https://hubzero.org/documentation/1.2.2/installation/Setup.openvz">1.2 Install Instructions</a>
+#   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.openvz">1.3 Install Instructions</a>
+# </ul>
 # <p><b>Note:</b> Needs a reboot after this has been installed. This is
 # currently handled by placing a <code>&& reboot</code> after the
 # <code>puppet apply</code> command...
-# <p>See: <a href= "https://hubzero.org/documentation/1.1.0/installation/Setup.openvz">https://hubzero.org/documentation/1.1.0/installation/Setup.openvz</a>
-class openvz {
-  package { "linux-image-2.6-openvz-amd64":
-    ensure => latest,
+class openvz(
+  $version
+) {
+
+  if $version == "1.1" {
+    package { "linux-image-2.6-openvz-amd64":
+      ensure => latest,
+    }
+  } else {
+    package { "hubzero-openvz":
+      ensure => latest,
+    }
+
+    exec { "initialize openvz":
+      command      => "/usr/bin/hzcms configure openvz --enable",
+      require      => [Package["hubzero-openvz"], Package["hubzero-cms"]],
+    }
   }
 
 # If we ever need to run the puppet scripts on startup we will have to
