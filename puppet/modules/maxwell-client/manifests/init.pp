ce95c704207797552e7c40d02f09dc7d02d3b139
Martin Paulo <martin.paulo@gmail.com>
2014-12-23 14:48:01 +1100

    Updated documentation

--- puppet/modules/maxwell-client/manifests/init.pp
+++ puppet/modules/maxwell-client/manifests/init.pp
@@ -4,7 +4,7 @@
 # <ul>
 #   <li><a href="https://hubzero.org/documentation/1.1.0/installation/Setup.maxwell_client">1.1 Install Instructions</a>
 #   <li><a href="https://hubzero.org/documentation/1.2.2/installation/Setup.maxwell_client">1.2 Install Instructions</a>
-#   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.firewall">1.3 Install Instructions</a>
+#   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.maxwell_client">1.3 Install Instructions</a>
 # </ul>
 class maxwell-client {
 
