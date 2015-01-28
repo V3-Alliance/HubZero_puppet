2c412ced37bc749b8c0c9033f662f2d21ef0aae3
Martin Paulo <martin.paulo@gmail.com>
2015-01-28 16:28:53 +1100

    Added a todo and some comments

--- puppet/modules/firewall/manifests/init.pp
+++ puppet/modules/firewall/manifests/init.pp
@@ -7,7 +7,9 @@
 #   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.firewall">1.3 Install Instructions</a>
 # </ul>
 class firewall {
-
+  # TODO: need to add the rule to let Nagios to be allowed to connect
+  # iptables -A INPUT -p tcp --dport 5666 -j ACCEPT
+  # to the last line of etc/firewall_on
   package { "hubzero-firewall":
     ensure => latest,
   }
