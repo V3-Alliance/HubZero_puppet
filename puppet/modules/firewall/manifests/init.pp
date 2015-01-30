1df881bee3a9d0d7728e7ec9e1019c380990f84a
Martin Paulo <martin.paulo@gmail.com>
2015-01-30 16:24:05 +1100

    First attempt to add Nagios port to firewall_on

--- puppet/modules/firewall/manifests/init.pp
+++ puppet/modules/firewall/manifests/init.pp
@@ -7,11 +7,16 @@
 #   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.firewall">1.3 Install Instructions</a>
 # </ul>
 class firewall {
-  # TODO: need to add the rule to let Nagios to be allowed to connect
-  # iptables -A INPUT -p tcp --dport 5666 -j ACCEPT
-  # to the last line of etc/firewall_on
   package { "hubzero-firewall":
     ensure => latest,
+  }
+
+  # need to add the following rule to let Nagios to be allowed to connect
+  # iptables -A INPUT -p tcp --dport 5666 -j ACCEPT
+  # to the last line of /etc/firewall_on
+  exec { "/bin/echo 'iptables -A INPUT -p tcp --dport 5666 -j ACCEPT' >> '/etc/firewall_on'":
+    unless => "/bin/grep -qFx 'iptables -A INPUT -p tcp --dport 5666 -j ACCEPT' '/etc/firewall_on'",
+    require => [Package["hubzero-firewall"]],
   }
 
 }
