17c44d07c177fb54b93e8eee8626b750af3ca8c4
Martin Paulo <martin.paulo@gmail.com>
2015-02-02 13:46:58 +1100

    Temporary commit to help with debugging

--- puppet/modules/hubzero_1_3/manifests/init.pp
+++ puppet/modules/hubzero_1_3/manifests/init.pp
@@ -2,31 +2,31 @@ class hubzero_1_3 {
   include mysql_hubzero
   include exim4
   include cms
-  include open-ldap
-  include webdav
-  include subversion
-  include trac
-  include forge
-  include openvz
-  include firewall
-  include maxwell-service
-  include maxwell-client
-  include vncproxy
-  include telequotad
-  include workspace
-  include metrics
-  include rappture
-  include filexfer
-# include submit
-# now the v3 support packages
-# first off, the nagios checks
-  include nrpe
-  include nrpe::absolutememory
-  include nrpe::apachememory
-# we want the machine to install security updates automatically.
-  include unattended_upgrades
-# we want to have V3 admins able to access the machine
-  include v3::admins
-# we want to have NTP
-  include '::ntp'
+#  include open-ldap
+#  include webdav
+#  include subversion
+#  include trac
+#  include forge
+#  include openvz
+#  include firewall
+#  include maxwell-service
+#  include maxwell-client
+#  include vncproxy
+#  include telequotad
+#  include workspace
+#  include metrics
+#  include rappture
+#  include filexfer
+## include submit
+## now the v3 support packages
+## first off, the nagios checks
+#  include nrpe
+#  include nrpe::absolutememory
+#  include nrpe::apachememory
+## we want the machine to install security updates automatically.
+#  include unattended_upgrades
+## we want to have V3 admins able to access the machine
+#  include v3::admins
+## we want to have NTP
+#  include '::ntp'
 }
