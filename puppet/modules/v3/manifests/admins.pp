b0255fcf49f9e36cba51a35490fffb6944e24dc7
Martin Paulo <martin.paulo@gmail.com>
2015-02-19 14:18:41 +1100

    Fixed the wrong case used to name the v3 module...

--- puppet/modules/v3/manifests/admins.pp
+++ puppet/modules/v3/manifests/admins.pp
@@ -64,6 +64,6 @@ class v3::admins {
     ensure => file,
     owner  => root,
     mode   => '0440',
-    source => 'puppet:///modules/V3/sudogroup',
+    source => 'puppet:///modules/v3/sudogroup',
   }
 }
