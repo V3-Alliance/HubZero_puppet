c39c5df62024d2d9cf8b8f1e52b230235c1ffdb8
Martin Paulo <martin.paulo@gmail.com>
2015-02-20 12:58:38 +1100

    Added the required bc package

--- puppet/modules/nrpe/manifests/apachememory.pp
+++ puppet/modules/nrpe/manifests/apachememory.pp
@@ -3,30 +3,33 @@
 
 class nrpe::apachememory {
 
-    # the script needs the bc package to be installed.
-    file {"/etc/nagios-plugins/config/apachememory.cfg":
-      ensure    => file,
-      source    => "puppet:///modules/nrpe/apachememory.cfg",
-      require   => Package ['nagios-nrpe-server'],
-      notify    => Service ['nagios-nrpe-server'],
-      mode      => '0644',
-    }
 
-    file {"/etc/nagios/nrpe.d/check_apachememory.cfg":
-      ensure    => file,
-      source    => "puppet:///modules/nrpe/check_apachememory.cfg",
-      require   => Package ['nagios-nrpe-server'],
-      notify    => Service ['nagios-nrpe-server'],
-      mode      => '0644',
-    }
+# the script needs the bc package to be installed.
+  file { "/etc/nagios-plugins/config/apachememory.cfg":
+    ensure    => file,
+    source    => "puppet:///modules/nrpe/apachememory.cfg",
+    require   => Package ['nagios-nrpe-server'],
+    notify    => Service ['nagios-nrpe-server'],
+    mode      => '0644',
+  }
+  ->
+  package { "bc":
+    ensure  => installed,
+  }
 
-    file {"/usr/lib/nagios/plugins/check_cpu_proc.sh":
-      ensure    => file,
-      source    => "puppet:///modules/nrpe/check_cpu_proc.sh",
-      mode      => '0755',
-      require   => Package ['nagios-nrpe-server'],
-      notify    => Service ['nagios-nrpe-server'],
-    }
+  file { "/etc/nagios/nrpe.d/check_apachememory.cfg":
+    ensure    => file,
+    source    => "puppet:///modules/nrpe/check_apachememory.cfg",
+    require   => Package ['nagios-nrpe-server'],
+    notify    => Service ['nagios-nrpe-server'],
+    mode      => '0644',
+  }
+
+  file { "/usr/lib/nagios/plugins/check_cpu_proc.sh":
+    ensure    => file,
+    source    => "puppet:///modules/nrpe/check_cpu_proc.sh",
+    mode      => '0755',
+    require   => Package ['nagios-nrpe-server'],
+    notify    => Service ['nagios-nrpe-server'],
+  }
 }
-
-		
