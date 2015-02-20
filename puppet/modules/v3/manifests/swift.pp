0dbcf7f3d28d5a9614aa356ab99259a856f126de
Martin Paulo <martin.paulo@gmail.com>
2015-02-20 16:35:43 +1100

    Corrected the duplicate package name.

--- puppet/modules/v3/manifests/swift.pp
+++ puppet/modules/v3/manifests/swift.pp
@@ -1,18 +1,17 @@
 class v3::swift (
 ){
 
-  $swift_requires = [ "python-pip", "python-dev" ]
-
-  package { $swift_requires:
-    ensure => "installed"
-  }
-  ->
-  exec { "keystone":
-    command => "/usr/bin/pip install python-keystoneclient",
-  }
-
-  exec { "swift":
-    command => "/usr/bin/pip install python-swiftclient",
-  }
+package { "python-pip":
+  ensure => "installed"
+}
+->
+exec { "keystone":
+  command => "/usr/bin/pip install python-keystoneclient",
+  require => Package["python-dev"],
+}
+->
+exec { "swift":
+  command => "/usr/bin/pip install python-swiftclient",
+}
 
 }
