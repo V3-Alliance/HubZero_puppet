5d9a57aeb6a8048010abf80841fddbb7af17cf4c
Martin Paulo <martin.paulo@gmail.com>
2015-02-23 16:12:14 +1100

    Trying take to at trying to deal with pips changes

--- puppet/modules/v3/manifests/swift.pp
+++ puppet/modules/v3/manifests/swift.pp
@@ -6,16 +6,19 @@ class v3::swift (
   }
   ->
   exec { "keystone":
-    # bizarro: this seems to install a new version of pip in a different location...
+  # bizarro: this seems to install a new version of pip in a different location...
     command => "pip install python-keystoneclient",
     require => Package["python-dev"],
+    path    => "/bin"
   }
   ->
   exec { "swift":
     command => "pip install python-swiftclient",
+    path    => "/bin"
   }
   ->
   exec { "distribute":
-     command => "pip install --upgrade distribute"
+    command => "pip install --upgrade distribute",
+    path    => "/bin"
   }
 }
