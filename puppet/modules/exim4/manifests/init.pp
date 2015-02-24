993bcc4884adc5d0de8bed5d9f63d275f27de1a4
Martin Paulo <martin.paulo@gmail.com>
2015-02-24 14:48:41 +1100

    Changed comments

--- puppet/modules/exim4/manifests/init.pp
+++ puppet/modules/exim4/manifests/init.pp
@@ -6,6 +6,9 @@
 #   <li><a href="https://hubzero.org/documentation/1.2.2/installation/Setup.mysql">1.2 Install Instructions</a>
 #   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.mysql">1.3 Install Instructions</a>
 # </ul>
+# Based on
+#     http://projects.puppetlabs.com/projects/1/wiki/debian_preseed_patterns
+#     http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/
 # Also see: <a href="https://www.digitalocean.com/community/tutorials/how-to-install-the-send-only-mail-server-exim-on-ubuntu-12-04">
 #     how to install the send only mail server exim on ubuntu 12-04</a>
 # After rebooting, it would seem that sometimes the exim4 package resets its configuration and no longer mails out
@@ -20,9 +23,6 @@
 class exim4 (
   $fqdn
 ){
-  # based on
-  #  http://projects.puppetlabs.com/projects/1/wiki/debian_preseed_patterns
-  #  http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/
 
   package { "exim4":
     require => File ["/var/cache/debconf/exim4.seeds"],
