abdfa8a09ca435f608c09c46a076efce1a904f43
Martin Paulo <martin.paulo@gmail.com>
2015-02-25 14:19:09 +1100

    first cut at some error checking...

--- puppet/modules/v3/files/mysql-backup-post.sh
+++ puppet/modules/v3/files/mysql-backup-post.sh
@@ -1,7 +1,24 @@
 #!/bin/bash -e
 
+function progress_check {
+    if [ $? -eq 0 ]
+    then
+        echo "Successfully backed up $1"
+    else
+        echo "Could not create $1 backup" >&2
+    fi
+}
+
 /etc/ldapbackup
+progress_check "ldap database"
+
 /etc/sitebackup
+progress_check "site files"
+
 /etc/userbackup
+progress_check "users"
+
 source /etc/nectar.secrets
 duplicity /mnt/backup/ swift://$(hostname)
+
+progress_check "swift"
