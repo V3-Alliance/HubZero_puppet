d16f71c2d5f94fb4198934eeaab08f1654a2078a
Martin Paulo <martin.paulo@gmail.com>
2015-02-26 11:03:30 +1100

    Updated documentation

--- puppet/modules/exim4/manifests/init.pp
+++ puppet/modules/exim4/manifests/init.pp
@@ -6,33 +6,38 @@
 #   <li><a href="https://hubzero.org/documentation/1.2.2/installation/Setup.mysql">1.2 Install Instructions</a>
 #   <li><a href="https://hubzero.org/documentation/1.3.0/installation/installdeb.mysql">1.3 Install Instructions</a>
 # </ul>
-# Based on
-#     http://projects.puppetlabs.com/projects/1/wiki/debian_preseed_patterns
-#     http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/
-# Also see: <a href="https://www.digitalocean.com/community/tutorials/how-to-install-the-send-only-mail-server-exim-on-ubuntu-12-04">
+# <p>Based on:
+# <ul>
+#   <li><a href="http://projects.puppetlabs.com/projects/1/wiki/debian_preseed_patterns">Debian preseed patterns</a>
+#   <li><a href="http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/">
+#          Dealing with the sun licence with debconf and preseeds</a>
+# </ul>
+# <p> Also see: <a href="https://www.digitalocean.com/community/tutorials/how-to-install-the-send-only-mail-server-exim-on-ubuntu-12-04">
 #     how to install the send only mail server exim on ubuntu 12-04</a>
-# After rebooting, it would seem that sometimes the exim4 package resets its configuration and no longer mails out
+# <p>After rebooting, it would seem that sometimes the exim4 package resets its configuration and no longer mails out.
 # The quick fix is to  run:
-#     dpkg-reconfigure exim4-config
+# <pre>dpkg-reconfigure exim4-config</pre>
 # and select the "internet site" option.
-# The exim -bV command gives useful information on exim's setup.
-# To test on the command line, enter the following (where someone@target.com is your email address!):
-#     $ echo “This is a test message.” | mail -s “Hello from hubzero” someone@target.com
-# To send a file via the command line, enter the following (where someone@target.com is your email address!):
-#     $ mail -s “A filey gift” someone@target.com < /a/path/to/some/file
+# <p>The
+# <pre>exim -bV command</pre>
+# <p>gives useful information on exim's setup.
+# <p>To test on the command line, enter the following (where someone@target.com is your email address!):
+# <pre>$ echo “This is a test message.” | mail -s “Hello from hubzero” someone@target.com</pre>
+# <p>To send a file via the command line, enter the following (where someone@target.com is your email address!):
+# <pre>$ mail -s “A filey gift” someone@target.com < /a/path/to/some/file</pre>
 class exim4 (
   $fqdn
 ){
 
   package { "exim4":
-    require => File ["/var/cache/debconf/exim4.seeds"],
+    require      => File ["/var/cache/debconf/exim4.seeds"],
     responsefile => "/var/cache/debconf/exim4.seeds",
-    ensure => latest;
+    ensure       => latest;
   }
 
-  file {"/var/cache/debconf/exim4.seeds":
+  file { "/var/cache/debconf/exim4.seeds":
     content => template ('exim4/exim4.seeds.erb'),
-    ensure => present;
+    ensure  => present;
   }
 
 }
