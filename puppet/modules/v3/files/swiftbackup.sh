11323ea5cdd97bc8bc6b85a05b9010733ef4eeab
Martin Paulo <martin.paulo@gmail.com>
2015-02-27 16:42:38 +1100

    Corrected time format

--- puppet/modules/v3/files/swiftbackup.sh
+++ puppet/modules/v3/files/swiftbackup.sh
@@ -3,4 +3,4 @@
 source /etc/nectar.secrets
 duplicity --volsize 100 /mnt/backup/ swift://$(hostname)
 
-duplicity remove-older-than 7d --force swift://$(hostname)
+duplicity remove-older-than 7D00s --force swift://$(hostname)
