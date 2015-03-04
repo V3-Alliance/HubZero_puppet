d5a89d815e807b9894914a4fa8be84995642e9cf
Martin Paulo <martin.paulo@gmail.com>
2015-03-04 11:59:50 +1100

    Updated so that egrep wouldn't cause the script to fail if there were no users within the UGIDLIMIT...

--- puppet/modules/v3/files/userbackup.sh
+++ puppet/modules/v3/files/userbackup.sh
@@ -6,11 +6,11 @@
 USER_BACKUP_FILE=users-$( date +%y%m%d-%H%M ).tar.gz
 BACKUP_DIR=/mnt/backup/users
 WORKING_DIR=$BACKUP_DIR/scratch
-UGIDLIMIT=500
+UGIDLIMIT=1000
 mkdir -p $WORKING_DIR
 awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > $WORKING_DIR/passwd.mig
 awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > $WORKING_DIR/group.mig
-awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - |egrep -f - /etc/shadow > $WORKING_DIR/shadow.mig
+awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - | egrep -f - /etc/shadow > $WORKING_DIR/shadow.mig || (( $?==1 ))
 cp /etc/gshadow $WORKING_DIR/gshadow.mig
 tar -zcpPf $WORKING_DIR/home.tar.gz /home
 tar -zcpPf $WORKING_DIR/mail.tar.gz /var/spool/mail
