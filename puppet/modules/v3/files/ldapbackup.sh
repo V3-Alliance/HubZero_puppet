289590ab3471184f486535f5efa5d0e1ccf63293
Martin Paulo <martin.paulo@gmail.com>
2015-03-06 11:56:33 +1100

    new ldap backup...

--- puppet/modules/v3/files/ldapbackup.sh
+++ puppet/modules/v3/files/ldapbackup.sh
@@ -1,6 +1,16 @@
 #!/bin/bash -e
-LDAP_BACKUP_FILE=ldap-$( date +%y%m%d-%H%M ).ldif
+
+#based on: https://help.ubuntu.com/lts/serverguide/openldap-server.html#ldap-backup
+
 BACKUP_DIR=/mnt/backup/ldap
-mkdir -p $BACKUP_DIR
-/usr/sbin/slapcat -v -l $BACKUP_DIR/$LDAP_BACKUP_FILE  > /dev/null
-gzip -9 $BACKUP_DIR/$LDAP_BACKUP_FILE
+WORKING_DIR=${BACKUP_DIR}/scratch
+LDAP_BACKUP_FILE=${BACKUP_DIR}/ldap-$( date +%y%m%d-%H%M ).tar.gz
+SLAPCAT=/usr/sbin/slapcat
+
+mkdir -p ${WORKING_DIR}
+${SLAPCAT} -n 0 > ${WORKING_DIR}/config.ldif
+${SLAPCAT} -n 1 > ${WORKING_DIR}/$(hostname).ldif
+cp /etc/nslcd.conf ${WORKING_DIR}
+
+tar -zcpPf ${LDAP_BACKUP_FILE} ${WORKING_DIR}
+rm -r ${WORKING_DIR}
