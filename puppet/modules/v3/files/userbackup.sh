1181aeb702909d5570d6a48e9b84d94c4ed4a66a
Martin Paulo <martin.paulo@gmail.com>
2015-03-06 11:27:33 +1100

    stopped using simple variable usage.

--- puppet/modules/v3/files/userbackup.sh
+++ puppet/modules/v3/files/userbackup.sh
@@ -4,14 +4,14 @@
 
 USER_BACKUP_FILE=users-$( date +%y%m%d-%H%M ).tar.gz
 BACKUP_DIR=/mnt/backup/users
-WORKING_DIR=$BACKUP_DIR/scratch
+WORKING_DIR=${BACKUP_DIR}/scratch
 UGIDLIMIT=1000
-mkdir -p $WORKING_DIR
-awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > $WORKING_DIR/passwd.mig
-awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > $WORKING_DIR/group.mig
-awk -v LIMIT=$UGIDLIMIT -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - | egrep -f - /etc/shadow > $WORKING_DIR/shadow.mig || (( $?==1 ))
-cp /etc/gshadow $WORKING_DIR/gshadow.mig
-tar -zcpPf $WORKING_DIR/home.tar.gz /home
-tar -zcpPf $WORKING_DIR/mail.tar.gz /var/spool/mail
-tar -zcpPf $BACKUP_DIR/$USER_BACKUP_FILE $WORKING_DIR
-rm -r $WORKING_DIR
+mkdir -p ${WORKING_DIR}
+awk -v LIMIT=${UGIDLIMIT} -F: '($3>=LIMIT) && ($3!=65534)' /etc/passwd > ${WORKING_DIR}/passwd.mig
+awk -v LIMIT=${UGIDLIMIT} -F: '($3>=LIMIT) && ($3!=65534)' /etc/group > ${WORKING_DIR}/group.mig
+awk -v LIMIT=${UGIDLIMIT} -F: '($3>=LIMIT) && ($3!=65534) {print $1}' /etc/passwd | tee - | egrep -f - /etc/shadow > ${WORKING_DIR}/shadow.mig || (( $?==1 ))
+cp /etc/gshadow ${WORKING_DIR}/gshadow.mig
+tar -zcpPf ${WORKING_DIR}/home.tar.gz /home
+tar -zcpPf ${WORKING_DIR}/mail.tar.gz /var/spool/mail
+tar -zcpPf ${BACKUP_DIR}/${USER_BACKUP_FILE} ${WORKING_DIR}
+rm -r ${WORKING_DIR}
