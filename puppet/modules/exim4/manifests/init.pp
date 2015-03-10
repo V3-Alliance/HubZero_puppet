c41792697da7481814525eadccd65bb187f07f95
Martin Paulo <martin.paulo@gmail.com>
2015-03-11 10:44:33 +1100

    Updated comments with latest learning

--- puppet/modules/exim4/manifests/init.pp
+++ puppet/modules/exim4/manifests/init.pp
@@ -12,6 +12,10 @@
 #   <li><a href="http://blogs.cae.tntech.edu/mwr/2008/02/05/stupid-puppet-trick-agreeing-to-the-sun-java-license-with-debconf-preseeds-and-puppet/">
 #          Dealing with the sun licence with debconf and preseeds</a>
 # </ul>
+# <p> Also note that the pre-seeding only works once: when the package is installed. After that, you have to
+# edit the config file directly and run <pre>update-exim4-config</pre> or do a <pre>dpkg-reconfigure exim4-config</pre>
+# <p> See <a href="http://serverfault.com/questions/614895/debconf-is-ignoring-my-default-anwsers">
+#           debconf is ignoring my default anwsers.</a>
 # <p> Also see: <a href="https://www.digitalocean.com/community/tutorials/how-to-install-the-send-only-mail-server-exim-on-ubuntu-12-04">
 #     how to install the send only mail server exim on ubuntu 12-04</a>
 # <p>After rebooting, it would seem that sometimes the exim4 package resets its configuration and no longer mails out.
@@ -29,15 +33,15 @@ class exim4 (
   $fqdn
 ){
 
+  file { "/var/cache/debconf/exim4.seeds":
+    content => template ('exim4/exim4.seeds.erb'),
+    ensure  => present;
+  }
+  ->
   package { "exim4":
     require      => File ["/var/cache/debconf/exim4.seeds"],
     responsefile => "/var/cache/debconf/exim4.seeds",
     ensure       => latest;
-  }
-
-  file { "/var/cache/debconf/exim4.seeds":
-    content => template ('exim4/exim4.seeds.erb'),
-    ensure  => present;
   }
 
 }
