c744e151093477f89cd2d5b3c482cbc023920650
Martin Paulo <martin.paulo@gmail.com>
2015-03-18 10:58:22 +1100

    Fixed up wrong class names

--- puppet/modules/v3/manifests/admins.pp
+++ puppet/modules/v3/manifests/admins.pp
@@ -11,25 +11,25 @@ class v3::admins {
     user_id    => '501',
     public_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDMW4zLFHV0j8GgVG/5yebcs95gaCBwgv+YfTLAok/dY77LoqVGhw+4vP3A03qnc0l08Dq5BJz8x6i4JSmUgYS+tMh7MIopR6VsG3jDwt7cViJUKicWEUCwF0M8RRtdLAtVvbv3o3DrhLcEqzpGE4yO3wIYkFzFvU+wmoA4IJt95ZK3z/VXtljUs4bB8XeYfAf+gGei/y9sNcn5388LDr5Wkqay/2fXmllC7yAxeNdD5pOsDI1/xwFBYCG5wmatsJvKL5zpWQPm1LWQ0uCPZzn40t5KsFGE5puxs6uvVSpmn3k/u7qlMfYhpDIXhn63yw9Kzi9qL2PFVsbmk7Y6Z+qp',
   }
-  class { 'v3::admins':
+  class { 'v3::admin_account':
     username   => 'alan',
     user_mail  => 'alan@v3.org.au',
     user_id    => '502',
     public_key => 'AAAAB3NzaC1yc2EAAAABIwAAAQEA7UQmx4+R7ToMxZJyQmfV0xIAQ1v3WYibFHKTmLd830HVOH+iP+vWTVqLd6eCU6gdV7RmnkgJ/LMsYZC48kguyN2YBzA3+vWSexAcrKxlcNchLAbw66fwTy54jzrACRDsJw+6cXUmox/ZKIs3LTsGHRC2ukE4GEO9OMsQOBL/Fhzkh1RcIlV87Tjl4zMg5Ojeav+63qh3jmyvj6DfBoXlvNoxG/9kewI7w132+FQSmQwq8E5WLLG3u989YWC/MOj0EWvkzjUoZD9OPvoxxZcxNFD5K9AZYq/FliGd7as8G8WbYwbf7Nl9xSgLLA8yHKDVaRkuP+qbZjp8yJSsV9YMLQ==',
   }
-  class { 'v3::admins':
+  class { 'v3::admin_account':
     username   => 'dmicevski',
     user_mail  => 'dmicevski@v3.org.au',
     user_id    => '503',
     public_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC4E2LNMEh9l0FO/vhzWnPnoY8Ov7HC8ie6djt+J64sVacLra/kPvUzNCosYVGCn/0QLSlU3fD//VFrGnpKCCgsbwmoLDeQwaSnS1pxluIdFXXySjZ+096hSvF5MrNy4lSPYixRx6vIBPJOWCiP0Tq0T7EpugnPhevpq7ZSXOviwTr6ek2ZmgoQlBb/KFP1+/qxUkQDnd5ylM+Iu6etuNEQ87Oei4TOZg1+6Q75Mo8j0KOyVVPsuOnorlknNA0/B6F+yzSeWgR+RqMD0e5u+FbpEyTgOTSw0OsfBPpfntNAGFarsZQydOgv7EkoL1n0NW0qFAb3Yg4eZV9w3ys5YL5X',
   }
-  class { 'v3::admins':
+  class { 'v3::admin_account':
     username   => 'melvin',
     user_mail  => 'melvin@v3.org.au',
     user_id    => '504',
     public_key => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQDIAxgXVuCzQavkX5WqDuo9zJ+PvI3dxGsh4mPPkwqQ4eotKNYnXiWmj9iDdIkFkWCTF2sy1oNhiR4U5tYQJJEEtmYl9w94aM0fnlB+0k7JTaFsMfk41Q2PoHYa0oLDMoZ2CFKTyKVCJDANjXGGYfQUhOz3W0q25KZmTEMfErwJwhMmEiyliZgGkjUHcKfEDBMaN/Dtn4t52RvDxCPs57zzT8A91XaxuqAKhdra8MZVigu6WStXVUqSya3C6TQo7t7Sj3JLldQSu+G4uDYAjP/22KG7b0+HcyyVCRgihky55vd0fipCHStIqXgNsAnJjbZM3ZiNJEn5l6FdMWFQvINV',
   }
-  class { 'v3::admins':
+  class { 'v3::admin_account':
     username   => 'jwinton',
     user_mail  => 'jwinton@v3.org.au',
     user_id    => '505',
