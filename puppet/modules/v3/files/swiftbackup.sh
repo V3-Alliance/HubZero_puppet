a1a14e19c3c3dff8bed14fe018012f77bace84be
Martin Paulo <martin.paulo@gmail.com>
2015-03-17 15:11:58 +1100

    Updated to delete backups older than a month, and to do a full backup every seven days

--- puppet/modules/v3/files/swiftbackup.sh
+++ puppet/modules/v3/files/swiftbackup.sh
@@ -1,6 +1,13 @@
 #!/bin/bash -e
 
+# values needed in a seperate file so we can source them from the command line as well
 source /etc/nectar.secrets
-duplicity --volsize 100 /mnt/backup/ swift://$(hostname)
 
-duplicity remove-older-than 7D00s --force swift://$(hostname)
+# perform a full backup every seven days
+duplicity --volsize 100 --full-if-older-than 7D /mnt/backup/ swift://$(hostname)
+
+# delete all backups older than a month
+duplicity remove-older-than 1M --force swift://$(hostname)
+
+unset SWIFT_PASSWORD
+unset PASSPHRASE
