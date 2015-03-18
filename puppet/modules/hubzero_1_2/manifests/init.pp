5da8e60e4f1c69356d3ed804b14d121fc94f04bd
Martin Paulo <martin.paulo@gmail.com>
2015-03-18 14:31:13 +1100

    Quick test to see which availablity zones allow email

--- puppet/modules/hubzero_1_2/manifests/init.pp
+++ puppet/modules/hubzero_1_2/manifests/init.pp
@@ -1,23 +1,23 @@
 class hubzero_1_2 {
-  include mysql_hubzero
+#  include mysql_hubzero
   include exim4
-  include cms
-  include open-ldap
-  include webdav
-  include subversion
-  include trac
-  include forge
-  include openvz
-  include firewall
-  include maxwell-service
-  include maxwell-client
-  include vncproxy
-  include telequotad
-  include workspace
-#  include metrics # metrics for 1.2.2 is broken, the advice is not to install it...
-  include rappture
-  include filexfer
-# include submit
-# now the v3 support packages
-  include v3::support
+#  include cms
+#  include open-ldap
+#  include webdav
+#  include subversion
+#  include trac
+#  include forge
+#  include openvz
+#  include firewall
+#  include maxwell-service
+#  include maxwell-client
+#  include vncproxy
+#  include telequotad
+#  include workspace
+##  include metrics # metrics for 1.2.2 is broken, the advice is not to install it...
+#  include rappture
+#  include filexfer
+## include submit
+## now the v3 support packages
+#  include v3::support
 }
