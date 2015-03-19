3a487e1fb357c079d851756d3d809a8860e1d4ae
Martin Paulo <martin.paulo@gmail.com>
2015-03-19 14:16:44 +1100

    Removed potentially uneeded host...

--- puppet/modules/nrpe/manifests/init.pp
+++ puppet/modules/nrpe/manifests/init.pp
@@ -74,7 +74,7 @@ class nrpe (
   }
 
   exec { 'create nagios mysql user':
-    command => "mysql -h localhost -u root -e \"GRANT SELECT ON example.* TO 'nagios'@'localhost' IDENTIFIED BY '${nagios_mysql_password}';\"",
+    command => "mysql -u root -e \"GRANT SELECT ON example.* TO 'nagios'@'localhost' IDENTIFIED BY '${nagios_mysql_password}';\"",
     path    => "/usr/local/bin:/usr/bin/",
     # following breaks 1.1 :(
     require => Package ['hubzero-mysql'],
