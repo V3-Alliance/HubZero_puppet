21d4af02ec1d3d4f9e66a3ae60562acbf312b1f5
Martin Paulo <martin.paulo@gmail.com>
2015-11-09 14:27:25 +1100

    Some spelling corrections...

--- puppet/modules/v3/files/swiftbackup.sh
+++ puppet/modules/v3/files/swiftbackup.sh
@@ -3,8 +3,8 @@
 # values needed in a seperate file so we can source them from the command line as well
 source /etc/nectar.secrets
 
-# keystone client writes warnings to syserr: which automysql backup then assumes to mean an error occured.
-# hence we trap the error stream to a variable, then, if duplicity failes, echo it back into the syserr stream.
+# keystone client writes warnings to syserr: which automysql backup then assumes to mean an error occurred.
+# hence we trap the error stream to a variable, then, if duplicity fails, echo it back into the syserr stream.
 # Otherwise we just move on.
 function do_with_captured_syserr {
     ERROR="$($1 2>&1 > /dev/null)"
